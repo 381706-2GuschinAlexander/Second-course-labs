3718067d5d38828506eec19db94ef452c82b3fba
Alexander Guschin <igushin1999@gmail.com>
2018-12-20 19:41:56 +0300

    For Travis

--- vectorlib/Matrix.h
+++ vectorlib/Matrix.h
@@ -19,15 +19,15 @@ template <class T>
 Matrix<T>::Matrix(int n):TVector<TVector<T> >(n)
 {
 	for(int i = 0; i < n; i++)
-		p[i] = TVector<T>(n - i);
+    this->p[i] = TVector<T>(n - i);
 }
 
 
 template <class T>
 Matrix<T>::Matrix(Matrix<T>& A):TVector<TVector<T> >(A.GetSize())
 {
-	for (int i = 0; i < l; i++)
-		p[i] = A.p[i];
+	for (int i = 0; i < this->l; i++)
+    this->p[i] = A.p[i];
 }
 
 
@@ -35,10 +35,10 @@ template <class T>
 Matrix<T>::Matrix(TVector<TVector<T> >& A) :TVector<TVector<T> >(A.GetSize())
 {
 	for (int i = 0; i < A.GetSize(); i++)
-		if (A[i].GetSize() != l - i)
+		if (A[i].GetSize() != this->l - i)
 			throw (1);
 	for (int i = 0; i < l; i++)
-		p[i] = A[i];
+    this->p[i] = A[i];
 }
 
 
@@ -46,11 +46,11 @@ Matrix<T>::Matrix(TVector<TVector<T> >& A) :TVector<TVector<T> >(A.GetSize())
 template <class T>
 bool  Matrix<T>::operator == (Matrix<T>& A)
 {
-	if (l != A.l)
+	if (this->l != A.l)
 		return false;
 
-	for (int i = 0; i < l; i++)
-		if (p[i] != A.p[i])
+	for (int i = 0; i < this->l; i++)
+		if (this->p[i] != A.p[i])
 			return false;
 
 	return true;
@@ -59,16 +59,16 @@ bool  Matrix<T>::operator == (Matrix<T>& A)
 template <class T>
 Matrix<T>&  Matrix<T>::operator = (Matrix& A)
 {
-	if (l != A.l)
+	if (this->l != A.l)
 	{
 		
-		delete[] p;
-		l = A.l;
-		p = new TVector<T>[l];
+		delete[] this->p;
+    this->l = A.l;
+    this->p = new TVector<T>[l];
 	}
 		
-	for (int i = 0; i < l; i++)
-		p[i] = A.p[i];
+	for (int i = 0; i < this->l; i++)
+    this->p[i] = A.p[i];
 
 	return *this;
 }
@@ -76,12 +76,12 @@ Matrix<T>&  Matrix<T>::operator = (Matrix& A)
 template <class T>
 Matrix<T> Matrix<T>::operator + (Matrix<T>& A)
 {
-	if (l != A.l)
+	if (this->l != A.l)
 		throw(1);
 
-	TVector<TVector<T> > temp(l);
-	for (int i = 0; i < l; i++)
-		temp[i] = p[i] + A.p[i];
+	TVector<TVector<T> > temp(this->l);
+	for (int i = 0; i < this->l; i++)
+		temp[i] = this->p[i] + A.p[i];
 
 	return temp;
 }
@@ -89,12 +89,12 @@ Matrix<T> Matrix<T>::operator + (Matrix<T>& A)
 template <class T>
 Matrix<T> Matrix<T>::operator - (Matrix<T>& A)
 {
-	if (l != A.l)
+	if (this->l != A.l)
 		throw(1);
 
-	Matrix<T> temp(l);
-	for (int i = 0; i < l; i++)
-		temp[i] = p[i] - A.p[i];
+	Matrix<T> temp(this->l);
+	for (int i = 0; i < this->l; i++)
+		temp[i] = this->p[i] - A.p[i];
 
 	return temp;
 }
@@ -102,14 +102,14 @@ Matrix<T> Matrix<T>::operator - (Matrix<T>& A)
 template <class T>
 Matrix<T> Matrix<T>::operator * (Matrix<T>& A)
 {
-	if (l != A.l)
+	if (this->l != A.l)
 		throw(1);
 
-	Matrix<T> temp(l);
-	for (int i = 0; i < l; i++)
-		for (int j = 0; j < l - i; j++)
-			for (int k = 0; k < l - (j + i); k++)
-				temp[i][j] += p[i][k] * A.p[k][j];
+	Matrix<T> temp(this->l);
+	for (int i = 0; i < this->l; i++)
+		for (int j = 0; j < this->l - i; j++)
+			for (int k = 0; k < this->l - (j + i); k++)
+				temp[i][j] += this->p[i][k] * A.p[k][j];
 
 	return temp;
 }
