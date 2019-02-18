19edc4eb7ddd49ed24e8151b3ef55a50c1101144
Alexander Guschin <igushin1999@gmail.com>
2019-02-18 13:08:52 +0300

    half of ArrList + some tests

--- arrlisttest/arrlist_test.cpp
+++ arrlisttest/arrlist_test.cpp
@@ -1,69 +1,27 @@
 #include <gtest.h>
-#include "List.h"
+#include "ArrList.h"
 
-TEST(List, can_create_list)
+TEST(ArrList, can_create_list)
 {
-  ASSERT_NO_THROW(List<int> A);
+  ASSERT_NO_THROW(ArrList<int> A(3));
 }
 
-TEST(List, can_ins_and_get_value_from_first_link)
+TEST(ArrList, throw_when_create_list_with_negative_lenhgt)
 {
-  List<int> A;
+  ASSERT_ANY_THROW(ArrList<int> A(-3));
+}
+
+TEST(ArrList, can_ins_and_get_value_from_first_link)
+{
+  ArrList<int> A(3);
   A.InsFirst(10);
-  EXPECT_EQ(A.GetValue(0), 10);
+  EXPECT_EQ(A.GetFirst(), 10);
 }
 
-TEST(List, can_ins_and_get_value_from_last_link)
+TEST(ArrList, can_ins_and_get_value_from_last_link)
 {
-  List<int> A;
-  A.InsFirst(10);
-  A.InsLast(5);
-  EXPECT_EQ(A.GetValue(1), 5);
-}
-
-TEST(List, can_ins_and_get_value_from_custom_link)
-{
-  List<int> A;
+  ArrList<int> A;
   A.InsFirst(10);
   A.InsLast(5);
-  A.InsCustom(3, 1);
-  A.InsCustom(2, 2);
-  EXPECT_EQ(A.GetValue(2), 2);
-}
-
-TEST(List, throw_when_wrong_pos)
-{
-  List<int> A;
-  A.InsFirst(10);
-  A.InsLast(5);
-  ASSERT_ANY_THROW(A.GetValue(100));
-}
-
-TEST(List, can_delete_first_link)
-{
-  List<int> A;
-  A.InsFirst(10);
-  A.InsLast(5);
-  A.DelFirst();
-  EXPECT_EQ(A.GetValue(0), 5);
-}
-
-TEST(List, can_delete_custom_link)
-{
-  List<int> A;
-  A.InsFirst(10);
-  A.InsCustom(12, 1);
-  A.InsLast(5);
-  A.DelCustom(1);
-  EXPECT_EQ(A.GetValue(1), 5);
-}
-
-TEST(List, can_delete_last_link)
-{
-  List<int> A;
-  A.InsFirst(10);
-  A.InsCustom(12, 1);
-  A.InsLast(5);
-  A.DelCustom(2);
-  EXPECT_EQ(A.GetValue(1), 12);
+  EXPECT_EQ(A.GetLast(), 5);
 }
