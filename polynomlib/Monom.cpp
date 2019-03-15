8beed79c6b56d37023d2197585b48272981d08c7
Alexander Guschin <igushin1999@gmail.com>
2019-03-15 21:58:18 +0300

    some tests, some methods...

--- polynomlib/Monom.cpp
+++ polynomlib/Monom.cpp
@@ -38,11 +38,104 @@ Monom::Monom(const Monom& A)
   con = A.con;
   pow = new int[n];
   for (int i = 0; i < n; i++)
-    pow = A.pow;
+    pow[i] = A.pow[i];
 }
 
 Monom::~Monom()
 {
   if (pow != NULL)
     delete pow;
+}
+
+int Monom::GetN()
+{
+  return n;
+}
+
+double Monom::GetCon()
+{
+  return con;
+}
+
+int* Monom::GetPow()
+{
+  /*
+  int* p = new int[n];
+  for (int i = 0; i < n; i++)
+    p[i] = pow[i];
+  return p;
+  */
+
+  return pow;
+}
+
+void Monom::SetN(int _n)
+{
+  int* tmp = new int[n];
+  for (int i = 0; i < n; i++)
+    tmp = pow;
+  delete pow;
+
+  pow = new int[_n];
+  int firstInd = (n > _n ? _n : n);
+  int secondInd = (n <= _n ? _n : n);
+
+  for (int i = 0; i < firstInd; i++)
+    pow[i] = tmp[i];
+  if(_n > n)
+    for (int i = firstInd; i < secondInd; i++)
+      pow[i] = 0;
+
+  delete tmp;
+}
+
+void Monom::SetCon(double _con)
+{
+  con = _con;
+}
+
+void Monom::SetPow(int* _pow)
+{
+  try
+  {
+    for (int i = 0; i < n; i++)
+    {
+      if (_pow[i] < 0)
+        throw(1);
+
+      pow[i] = _pow[i];
+    }
+  }
+  catch (int e)
+  {
+    throw(e);
+  }
+}
+
+Monom& Monom::operator=(const Monom& A)
+{
+  if (pow != NULL)
+    delete pow;
+  n = A.n;
+  con = A.con;
+  pow = new int[A.n];
+  for (int i = 0; i < n; i++)
+    pow[i] = A.pow[i];
+
+  return *this;
+}
+
+Monom Monom::operator+(const Monom& A)
+{
+  
+  if (n != A.n)
+    throw(1);
+
+  Monom res(*this);
+  for (int i = 0; i < n; i++)
+    if (pow[i] != A.pow[i])
+      throw(1);
+
+  res.con += A.con;
+  return res;
 }
--- polynomtest/polynom_test.cpp
+++ polynomtest/polynom_test.cpp
@@ -1,3 +1,50 @@
 #include <gtest.h>
 #include "Polynom.h"
 
+TEST(Monom, can_create_monom)
+{
+  ASSERT_NO_THROW(Monom());
+}
+
+TEST(Monom, can_create_monom_with_valid_attributes)
+{
+  int p[] = { 3, 4, 5 };
+  ASSERT_NO_THROW(Monom(3, 4.5, p));
+}
+
+TEST(Monom, can_create_monom_with_negative_powers)
+{
+  int p[] = { -3, -4, -5};
+  ASSERT_ANY_THROW(Monom(3, 4.5, p));
+}
+
+TEST(Monom, can_create_monom_with_invalid_powers)
+{
+  int p[] = {3, 4};
+  ASSERT_ANY_THROW(Monom(3, 4.5, p));
+}
+
+TEST(Monom, can_create_monom_with_negative_n)
+{
+  int p[] = { 3, 4, 5 };
+  ASSERT_ANY_THROW(Monom(-3, 4.5, p));
+}
+
+TEST(Monom, can_create_monom_with_monom)
+{
+  int p[] = { 3, 4, 5};
+  Monom A(3, 4.5, p);
+  
+  Monom B(A);
+
+  bool res = true;
+  if (B.GetCon() != A.GetCon())
+    res = false;
+  if (B.GetN() != A.GetN())
+    res = false;
+  for(int i = 0; i < A.GetN(); i++)
+    if ((B.GetPow())[i] != 3 + i)
+      res = false;
+
+  EXPECT_EQ(res, 1);
+}
