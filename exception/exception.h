6c3827e55a82b5d0f104a8351d70c13a6bdba756
Alexander Guschin <igushin1999@gmail.com>
2019-03-28 22:31:38 +0300

    List_lab  + main

--- exception/exception.h
+++ exception/exception.h
@@ -1,9 +1,10 @@
-#ifndef EXP
+#ifndef __EXP
+#define __EXP
 
 #define __NEG_SIZE "Negative lenght/size"
 #define __IND_IS_OUT_OF_RANGE "Index is out of range"
-#define __STACK_IS_FULL "Attempt to put in full stack/queue/list" 
-#define __STACK_IS_EMPTY "Attempt to get from empty stack/queue/list"
+#define __STACK_IS_FULL "Attempt to put in SoD" 
+#define __STACK_IS_EMPTY "Attempt to get from empty SoD"
 #define __DIFF_SIZE "Attempt to interact with object with different size" 
 #define __INVALID_MATRIX "Invalid Object"
 #define __INVALID_SIZE_CTL "Invalid size compare to length"
--- list/main.cpp
+++ list/main.cpp
@@ -1,3 +1,52 @@
 #include <iostream>
-#include "Stack.h"
+#include "List.h"
+#include <time.h> 
 
+#define __TIME
+int main()
+{
+#ifndef __TIME
+  List<double> B, C, D;
+  for (int i = 0; i < 1000000; i++)
+    B.InsFirst(1.0);
+  for (int i = 0; i < 2000000; i++)
+    C.InsFirst(1.0);
+  for (int i = 0; i < 3000000; i++)
+    D.InsFirst(1.0);
+
+  float fTimeStart = clock()/(float)CLOCKS_PER_SEC;
+  B.InsCustom(1.12, 1000000 - 1);
+  float fTimeStop = clock()/ (float)CLOCKS_PER_SEC;
+  std::cout << "time: " << fTimeStop - fTimeStart << "\n";
+
+  fTimeStart = clock() / (float)CLOCKS_PER_SEC;
+  C.InsCustom(1.12, 2000000 - 1);
+  fTimeStop = clock() / (float)CLOCKS_PER_SEC;
+  std::cout << "time: " << fTimeStop - fTimeStart << "\n";
+
+  fTimeStart = clock() / (float)CLOCKS_PER_SEC;
+  D.InsCustom(1.12, 3000000 - 1);
+  fTimeStop = clock() / (float)CLOCKS_PER_SEC;
+  std::cout << "time: " << fTimeStop - fTimeStart << "\n";
+#endif
+  List<double> A;
+  std::cout << "Insert on first pos 1.1\n";
+  A.InsFirst(1.1);
+  std::cout << "Insert on last pos 2.2\n";
+  A.InsLast(2.2);
+  std::cout << "Insert on 1 pos 1.15\n";
+  A.InsCustom(1.15, 1);
+
+  std::cout << "length: " << A.GetLen() << "\n";
+  for (int i = 0; i < A.GetLen(); i++)
+    std::cout << i<<" pos: " << A.GetValue(i) << "\n";
+
+  std::cout << "delete pos 1\n";
+  A.DelCustom(1);
+
+  std::cout << "length: " << A.GetLen() << "\n";
+  for (int i = 0; i < A.GetLen(); i++)
+    std::cout << i << " pos: " << A.GetValue(i) << "\n";
+
+  return 0;
+}
